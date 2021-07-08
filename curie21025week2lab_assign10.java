7aff741842f157f8eded20f1c77a5ff27a892dfd
Safiya Parveen A R <86617526+CURIE21025@users.noreply.github.com>
2021-07-09 00:05:27 +0530

    Update curie21025week2lab_assign10.java

--- curie21025week2lab_assign10.java
+++ curie21025week2lab_assign10.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class curie21025week2lab_assign10 {
 
 	public static void main(String[] args) {
-		// odd or even
+		// odd or even if else
 		Scanner scan=new Scanner(System.in);
 		System.out.println("enter a number:");
 		int num=scan.nextInt();
@@ -13,6 +13,26 @@ public class curie21025week2lab_assign10 {
 			System.out.println(num+" is even");
 		else
 			System.out.println(num+" is odd");
+		
+		
+		
+		//ternary
+		System.out.println((num%2==0)?"ternary even":"ternary odd");
+		
+		//switch
+		switch(num%2)
+		{
+		 case 0:{ System.out.println("switch even");
+		  break;
+		 }
+		
+		 case 1:{System.out.println("switch odd");
+		  break;
+		 }
+		}
+
+	}
+		
 
 	}
 
